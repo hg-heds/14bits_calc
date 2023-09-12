6f6a506184563edcd769832d5410ff38150b8d74
hg-heds <hugo.hedsts.u@gmail.com>
2023-09-12 12:49:26 -0300

    readsrev

--- Avionics and Electronics/prs_readsinrev.ino.cpp
+++ Avionics and Electronics/prs_readsinrev.ino.cpp
@@ -1,9 +1,11 @@
-#define BLADE_COUNT 3
+#define BLADE_COUNT 2
 #define SAMPLE_SIZE 80
-long count_semirev = 0;
+int count_semirev = 0;
 bool history = false;
 bool signal = false;
 long ms;
+long reads_in_semirev = 0;
+long reads_in_semirev_history = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -13,16 +15,19 @@ void setup() {
 
 void loop() {
   signal = digitalRead(9);
+  reads_in_semirev++;
   if (signal == true && history == false){
+    reads_in_semirev_history = (reads_in_semirev_history*count_semirev+reads_in_semirev)/(count_semirev+1);
     count_semirev++;
+    reads_in_semirev = 0;
   }
   history = signal;
   if (count_semirev == SAMPLE_SIZE){
     float secs = ((float)(millis()-ms))/1000;
     Serial.print(millis());
-    Serial.print("    ");
-    Serial.print(secs);
-    Serial.print("    ");
+    Serial.print(F("    "));
+    Serial.print(reads_in_semirev_history);
+    Serial.print(F("    "));
     Serial.println(60*(float)count_semirev/secs/BLADE_COUNT,4);
     count_semirev = 0;
     ms = millis();
